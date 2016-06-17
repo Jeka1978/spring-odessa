9c1307b2ddcf0e4a4ff765993e38cd65c5f977da
bsevgeny@gmail.com <papakita2009>
2016-06-17 11:39:04 +0300

    object configurator created

---
+++ src/main/java/mySpring/InjectRandomIntAnnotationObjectConfigurator.java
@@ -0,0 +1,26 @@
+package mySpring;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+/**
+ * Created by Evegeny on 17/06/2016.
+ */
+public class InjectRandomIntAnnotationObjectConfigurator implements ObjectConfigurator {
+    @Override
+    public void configure(Object t) throws Exception {
+        Class<?> type = t.getClass();
+        Field[] fields = type.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(InjectRandomInt.class)) {
+                InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int randomNum = min + random.nextInt(max - min);
+                field.setAccessible(true);
+                field.set(t,randomNum);
+            }
+        }
+    }
+}
--- src/main/java/mySpring/ObjectFactory.java
+++ src/main/java/mySpring/ObjectFactory.java
@@ -1,26 +1,66 @@
 package mySpring;
 
+import org.reflections.Reflections;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 /**
  * Created by Evegeny on 17/06/2016.
  */
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
     private Config config = new JavaConfig();
+    private List<ObjectConfigurator> objectConfigurators = new ArrayList<>();
+    private Reflections reflections = new Reflections();
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
     private ObjectFactory() {
+        Set<Class<? extends ObjectConfigurator>> classes = reflections.getSubTypesOf(ObjectConfigurator.class);
+        for (Class<? extends ObjectConfigurator> aClass : classes) {
+            if (!Modifier.isAbstract(aClass.getModifiers())) {
+                try {
+                    objectConfigurators.add(aClass.newInstance());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public <T> T createObject(Class<T> type) throws Exception {
+        type = resolveImpl(type);
+        T t = type.newInstance();
+        configure(t);
+        return t;
+    }
+
+
+
+
+    private <T> void configure(T t) throws Exception {
+        for (ObjectConfigurator objectConfigurator : objectConfigurators) {
+            objectConfigurator.configure(t);
+        }
+    }
+
+    private <T> Class<T> resolveImpl(Class<T> type) {
         if (type.isInterface()) {
             type = config.getImpl(type);
         }
-        T t = type.newInstance();
-
-
-        return t;
+        return type;
     }
 }
+
+
+
+
+
+
