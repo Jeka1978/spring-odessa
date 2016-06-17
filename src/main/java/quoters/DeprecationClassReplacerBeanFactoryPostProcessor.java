dd44e5348352a7c46e4b6ef6094bbfd1cc3f31b1
bsevgeny@gmail.com <papakita2009>
2016-06-17 18:13:00 +0300

    write bfpp which will replace in bean defenitions deprecated classes Solved

--- src/main/java/quoters/DeprecationClassReplacerBeanFactoryPostProcessor.java
+++ src/main/java/quoters/DeprecationClassReplacerBeanFactoryPostProcessor.java
@@ -4,6 +4,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+
+import java.util.Map;
 
 /**
  * Created by Evegeny on 17/06/2016.
@@ -14,7 +18,21 @@ public class DeprecationClassReplacerBeanFactoryPostProcessor implements BeanFac
         String[] names = beanFactory.getBeanDefinitionNames();
         for (String name : names) {
             BeanDefinition beanDefinition = beanFactory.getBeanDefinition(name);
-            beanDefinition.getBeanClassName()
+            String beanClassName = beanDefinition.getBeanClassName();
+            Class<?> beanClass = ClassUtils.resolveClassName(beanClassName, ClassLoader.getSystemClassLoader());
+            DeprecatedClass annotation = beanClass.getAnnotation(DeprecatedClass.class);
+            if (annotation != null) {
+                Class alternativeClass = annotation.value();
+                beanDefinition.setBeanClassName(alternativeClass.getName());
+            }
+
         }
     }
 }
+
+
+
+
+
+
+
